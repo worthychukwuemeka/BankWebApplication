aeaed0c04a7046a3f4e497a1540bec64cd305c15
worrrthy <worthy.chukwuemeka@stu.cu.edu.ng>
2021-09-16 23:12:10 +0100

    Add project files.

---
+++ BankApplication/Areas/Identity/Data/BankDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BankApplication.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankApplication.Data
+{
+    public class BankDbContext : IdentityDbContext<BankApplicationUser>
+    {
+        public BankDbContext(DbContextOptions<BankDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ BankApplication/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using BankApplication.Areas.Identity.Data;
+using BankApplication.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BankApplication.Areas.Identity.IdentityHostingStartup))]
+namespace BankApplication.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<BankDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("BankDbContextConnection")));
+
+                services.AddDefaultIdentity<BankApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<BankDbContext>();
+            });
+        }
+    }
+}
